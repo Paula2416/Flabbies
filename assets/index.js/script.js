f418b7df62ba36bafe52320e9437972b94043e28
Paula Blessing Okello <vtxapzjo@students.codeinstitute.net>
2024-08-23 01:15:37 +0000

    Added function to randomly place flabby image in a bear slot.

--- assets/index.js/script.js
+++ assets/index.js/script.js
@@ -1,3 +1,6 @@
+let currFlabbyBear;
+
+
 window.onload = function() {
     setGame();
 }
@@ -7,4 +10,22 @@ function setGame() {
         bear.id = i.toString();
         document.getElementById("board").appendChild(bear);
     }
+    setInterval(setMole, 1000); 
+    setInterval(setPlant, 2000); 
 }
+
+function getRandomBear()
+{
+    let num = Math.floor(Math.random() *9);
+    return num.toString();
+}
+function setFlabby() {
+    let flabby = document.createElement("img");
+    flabby.src = "./flabby.png";
+
+    let num = getRandomBear();
+    currFlabbyBear = document.getElementById(num);
+    currFlabbyBear.appendChild(flabby);
+}
+
+
